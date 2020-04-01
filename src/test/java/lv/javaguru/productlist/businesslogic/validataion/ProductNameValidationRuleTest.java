991359b5c6ccc71a343c0008bd7fa35d79a2ae38
xmagwajx@gmail.com <Slayer1987###>
2020-04-01 22:08:16 +0300

    Adding tests. GetProductByCategoryService - on hold.

--- src/test/java/lv/javaguru/productlist/businesslogic/validataion/ProductNameValidationRuleTest.java
+++ src/test/java/lv/javaguru/productlist/businesslogic/validataion/ProductNameValidationRuleTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.math.BigDecimal;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ProductNameValidationRuleTest {
 
@@ -17,5 +18,11 @@ public class ProductNameValidationRuleTest {
     public void returnFalseWhenNameIsEmpty() {
         Product product = new Product(null, null, BigDecimal.valueOf(10), BigDecimal.valueOf(10), Category.FRUIT);
         assertFalse(rule.isValid(product));
+    }
+
+    @Test
+    public void returnTrueWhenNameIsFiled() {
+        Product product = new Product("Banana", null, BigDecimal.valueOf(20), BigDecimal.valueOf(10), Category.FRUIT);
+        assertTrue(rule.isValid(product));
     }
 }
